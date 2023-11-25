d8606e08da7531ede5f15d3f0f57333729e0242e
ameralnas <amer.live477@gmail.com>
2023-11-25 17:14:09 +0200

    Try now

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,25 +2,35 @@
 
 void swapping(int *, int *);
 
+/**
+ * selection_sort - A function to sort elements using selection sort
+ * @array: A pointer to an array of integers
+ * @size: The size of the array
+ *
+ * Return: Nothing (void)
+*/
+
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, myMin;
+	size_t i, j, myMin;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        myMin = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[myMin] > array[j])
-                myMin = j;
-            if (myMin != i)
-            {
-                swapping(&array[i], &array[myMin]);
-                print_array(array, size);
-            }
-        }
-    }
-    
+	if (array == NULL)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		myMin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[myMin] > array[j])
+				myMin = j;
+			if (myMin != i)
+			{
+				swapping(&array[i], &array[myMin]);
+				print_array(array, size);
+			}
+		}
+	}
+
 }
 
 /* 1 9 4 5 7 */
