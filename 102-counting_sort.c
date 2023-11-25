f013b71de19fb3bf9e7689abc3fdfb0f1897ae41
ameralnas <amer.live477@gmail.com>
2023-11-26 00:06:34 +0200

    Much better

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,34 +10,34 @@
 
 void counting_sort(int *array, size_t size)
 {
-    int maxElement = -20000;
-    int i, mySize = size;
-    int *myCountArray;
-    int *myOutputArray;
+	int maxElement = -20000;
+	int i, mySize = size;
+	int *myCountArray;
+	int *myOutputArray;
 
-    for (i = 0; i < mySize; i++)
-    {
-        if (maxElement < array[i])
-            maxElement = array[i];
-    }
-    myCountArray = (int *)malloc((maxElement + 1) * sizeof(int));
-    if (myCountArray == NULL)
-        return;
-    for (i = 0; i <= maxElement; i++)
-        myCountArray[i] = 0;
-    for (i = 0; i < mySize; i++)
-        myCountArray[array[i]]++;
-    for (i = 1; i <= maxElement; i++)
-        myCountArray[i] += myCountArray[i - 1];
-    print_array(myCountArray, maxElement + 1);
-    myOutputArray = (int *)malloc((mySize + 1) * sizeof(int));
-    for (i = mySize - 1; i >= 0; i--)
-    {
-        myOutputArray[myCountArray[array[i]] - 1] = array[i];
-        myCountArray[array[i]]--;
-    }
-    for (i = 0; i < mySize; i++)
-        array[i] = myOutputArray[i];
-    free(myCountArray);
-    free(myOutputArray);
+	for (i = 0; i < mySize; i++)
+	{
+		if (maxElement < array[i])
+			maxElement = array[i];
+	}
+	myCountArray = (int *)malloc((maxElement + 1) * sizeof(int));
+	if (myCountArray == NULL)
+		return;
+	for (i = 0; i <= maxElement; i++)
+		myCountArray[i] = 0;
+	for (i = 0; i < mySize; i++)
+		myCountArray[array[i]]++;
+	for (i = 1; i <= maxElement; i++)
+		myCountArray[i] += myCountArray[i - 1];
+	print_array(myCountArray, maxElement + 1);
+	myOutputArray = (int *)malloc((mySize + 1) * sizeof(int));
+	for (i = mySize - 1; i >= 0; i--)
+	{
+		myOutputArray[myCountArray[array[i]] - 1] = array[i];
+		myCountArray[array[i]]--;
+	}
+	for (i = 0; i < mySize; i++)
+		array[i] = myOutputArray[i];
+	free(myCountArray);
+	free(myOutputArray);
 }
